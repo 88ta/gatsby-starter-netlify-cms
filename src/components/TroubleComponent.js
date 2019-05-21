dff035f43cf03e15648b94a7748ae412fb8c38a2
88ta <macyahashun@gmail.com>
2019-05-21 20:50:29 +0900

    Define trouble component

---
+++ src/components/TroubleComponent.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	margin: auto;
+	padding: 64px 0;
+	width: 900px;
+
+  margin-top: 32px;
+  width: inherit;
+  justify-content: space-between;
+`
+
+const Inner = styled.div`
+  display: flex;
+  margin-top: 32px;
+  width: inherit;
+  justify-content: space-between;
+
+`
+
+const Cell = styled.div`
+  width: 240px;
+`
+
+const Img = styled.h3`
+  display: block;
+  width: 112px;
+  margin: 0 auto;
+  text-align: center;
+`
+
+const Text = styled.p`
+  margin-top: 32px;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 32px;
+  color: #5F4B45;
+`
+
+const TroubleComponent = () => (
+	<Wrapper id="trouble">
+		<h2>こんなお悩み、ありませんか？</h2>
+		<Inner>
+			<Cell id="cT1">
+				<Img src="img/cT1.png" />
+				<Text>"アカウント運用をしているが、フォロワー数が伸びない"</Text>
+			</Cell>
+			<Cell id="cT2">
+				<Img src="img/cT2.png" />
+				<Text>"効果的な運用方法がわからない"</Text>
+			</Cell>
+			<Cell id="cT3">
+				<Img src="img/cT3.png" />
+				<Text>"Instagramをビジネスに活用したいが、リソースが足りない"</Text>
+			</Cell>
+		</Inner>
+	</Wrapper>
+)
+
+export default TroubleComponent
