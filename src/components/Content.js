60381341ebdd68409d7729020ebd3fab5cf9305d
88ta <macyahashun@gmail.com>
2019-05-22 08:52:44 +0900

    Apply meta for article

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,56 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-component'
+
+const Wrapper = styled.div`
+  margin: auto;
+  width: 640px;
+  padding: 24px 0 64px;
+  h1 {
+    font-size: 24px;
+    line-height: 32px;
+    text-align: left;
+    color: #333  
+  }
+  h2 {
+    font-size: 24px;
+    font-weight: 600;
+    text-align: left;
+    margin-top: 48px;
+    color: #333;
+
+    &:after {
+      content: "";
+      display: block;
+      position: relative;
+      margin-top: 6px;
+      width: 100%;
+      height: 3px;
+      border: none;
+      border-radius: 2px;
+      background-color: #333;
+    }
+  }
+  img {
+    margin-top: 12px;
+    width: 640px;
+    height: 320px
+  }
+  p {
+    margin-top: 24px;
+    font-size: 16px;
+    text-align: left;
+    line-height: 28px;
+    color: #333;
+  }
+`
 
 export const HTMLContent = ({ content, className }) => (
   <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
 )
 
 const Content = ({ content, className }) => (
-  <div className={className}>{content}</div>
+  <Wrapper className={className}>{content}</Wrapper>
 )
 
 Content.propTypes = {
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+
+const H1 = styled.h1`
+  font-size: 24px;
+  line-height: 32px;
+  text-align: left;
+  color: #333;
+`
+
+const Container = styled.div`
+  margin: auto;
+  width: 640px;
+`
 
 export const BlogPostTemplate = ({
   content,
@@ -17,32 +29,35 @@ export const BlogPostTemplate = ({
   const PostContent = contentComponent || Content
 
   return (
-    <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </section>
+    <React.Fragment>
+      <Helmet>
+        <meta charset="UTF-8"/>
+        <meta http-equiv='CONTENT-LANGUAGE' content="ja"/>
+        <title>Instagram運用代行 - Instagram（インスタグラム）に特化したプロモーション支援のFinT -</title>
+        <meta name="description" content="Sucleの運営会社FinTは、instagramのフォロワー数・認知度・売上の向上を アカウント運用によってサポートします"/>
+        <meta name='KEYWORDS' content="インスタグラム,instagram,運用,代行,FinT"/>
+        <meta name="viewport" content="width=1280"/>
+        
+        <meta property="og:locale" content="ja_JP"/>
+        <meta property = "og:site_name" content = "FinTインスタグラム運用代行" />
+        <meta property="og:title" content="Instagram運用代行 - Instagram（インスタグラム）に特化したプロモーション支援のFinT -"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://fint-instagram.netlify.com/"/>
+        <meta property="og:image" content="https://fint-instagram.netlify.com/img/ogp.jpg"/>
+        <meta property="og:description" content="Sucleの運営会社FinTは、instagramのフォロワー数・認知度・売上の向上をアカウント運用によってサポートします" />
+        
+        <meta content="summary_large_image" name="twitter:card"/>
+        <meta content="FinTインスタグラム運用代行" name="twitter:site"/>
+        <meta content="Instagram運用代行 - Instagram（インスタグラム）に特化したプロモーション支援のFinT -" name="twitter:title"/>
+        <meta content="fint-instagram.netlify.com/" name="twitter:url"/>
+        <meta content="Sucleの運営会社FinTは、instagramのフォロワー数・認知度・売上の向上をアカウント運用によってサポートします" name="twitter:description"/>
+        <meta content="https://fint-instagram.netlify.com/img/ogp.jpg" name="twitter:image"/>
+      </Helmet>
+      <Container>
+        <H1>{title}</H1>
+        <PostContent content={content} />
+      </Container>
+    </React.Fragment>
   )
 }
 
