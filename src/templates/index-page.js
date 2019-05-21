e051db4b6d84ebfa1f87f61168099e9a9a18d800
88ta <macyahashun@gmail.com>
2019-05-22 08:25:59 +0900

    Apply meta for index

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import { Helmet } from "react-helmet"
 
 import Layout from '../components/Layout'
 import TopSection from '../components/TopSection'
@@ -22,7 +23,30 @@ export const IndexPageTemplate = ({
   description,
   intro,
 }) => (
-  <div>
+  <React.Fragment>
+    <Helmet>
+      <meta charset="UTF-8"/>
+      <meta http-equiv='CONTENT-LANGUAGE' content="ja"/>
+      <title>Instagram運用代行 - Instagram（インスタグラム）に特化したプロモーション支援のFinT -</title>
+      <meta name="description" content="Sucleの運営会社FinTは、instagramのフォロワー数・認知度・売上の向上を アカウント運用によってサポートします"/>
+      <meta name='KEYWORDS' content="インスタグラム,instagram,運用,代行,FinT"/>
+      <meta name="viewport" content="width=1280"/>
+      
+      <meta property="og:locale" content="ja_JP"/>
+      <meta property="og:site_name" content="FinTインスタグラム運用代行"/>
+      <meta property="og:title" content="Instagram運用代行 - Instagram（インスタグラム）に特化したプロモーション支援のFinT -"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://fint-instagram.netlify.com/"/>
+      <meta property="og:image" content="https://fint-instagram.netlify.com/img/ogp.jpg"/>
+      <meta property="og:description" content="Sucleの運営会社FinTは、instagramのフォロワー数・認知度・売上の向上を アカウント運用によってサポートします"/>
+      
+      <meta content="summary_large_image" name="twitter:card"/>
+      <meta content="FinTインスタグラム運用代行" name="twitter:site"/>
+      <meta content="Instagram運用代行 - Instagram（インスタグラム）に特化したプロモーション支援のFinT -" name="twitter:title"/>
+      <meta content="fint-instagram.netlify.com/" name="twitter:url"/>
+      <meta content="Sucleの運営会社FinTは、instagramのフォロワー数・認知度・売上の向上を アカウント運用によってサポートします" name="twitter:description"/>
+      <meta content="https://fint-instagram.netlify.com/img/ogp.jpg" name="twitter:image"/>
+    </Helmet>
     <TopSection />
     <TroubleSection />
     <CountonSection />
@@ -32,7 +56,7 @@ export const IndexPageTemplate = ({
     <IntroSection />
     <PlanSection />
     <FlowSection />
-  </div>
+  </React.Fragment>
 )
 
 IndexPageTemplate.propTypes = {
