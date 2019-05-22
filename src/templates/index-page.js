5d12bf8a1eda0c0d7b3946238ca671f68abb3a76
88ta <macyahashun@gmail.com>
2019-05-22 14:35:44 +0900

    WIP

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -46,6 +46,7 @@ export const IndexPageTemplate = ({
       <meta content="Sucleの運営会社FinTは、instagramのフォロワー数・認知度・売上の向上を アカウント運用によってサポートします" name="twitter:description"/>
       <meta content="https://fint-instagram.netlify.com/img/ogp.jpg" name="twitter:image"/>
     </Helmet>
+    <div>{title}</div>
     <TopSection />
     <TroubleSection />
     <CountonSection />
@@ -95,6 +96,45 @@ IndexPage.propTypes = {
     }),
   }),
 }
+
+export const pageQuery = graphql `
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        subheading
+        mainpitch {
+          title
+          description
+        }
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+      }
+    }
+  }
+`
+
 
 export default IndexPage
 
