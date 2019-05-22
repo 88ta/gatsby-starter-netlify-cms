ea147fe06f9d320832ddaea58697b2b80a5ba215
88ta <macyahashun@gmail.com>
2019-05-22 12:02:43 +0900

    Fix typo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import styled from 'styled-reset'
+import styled from 'styled-components'
 import { Reset } from 'styled-reset'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
